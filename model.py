d4634ad35cd5a9c955aeb78581950024b6393d23
ban-masa <bowofvinderre@gmail.com>
2019-08-15 12:41:45 +0900

    hoge

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 m1 = 1.0
 m2 = 1.0
-k = 10.0
+k = 100.0
 dt = 0.01
-d = 0.1
+d = 1.0
 
 A = np.array([[1.0, dt, 0.0, 0.0],
               [-k * dt / m1, 1.0, k * dt / m1, 0.0],
@@ -25,20 +25,34 @@ def test1():
     timelist = []
     datalist = []
     x = np.array([[0, 0, 0, 0]]).T
-    for i in range(1000):
+    for i in range(10000):
         timelist.append(i * dt)
         u = -1 * (x[2][0] - 5.0)
         x = calc_next(x, u)
         datalist.append(x[2][0])
-    print datalist
-    plt.plot(timelist, datalist, label="test")
+    plt.plot(timelist, datalist, label="test1")
     plt.legend()
     plt.show()
 
-def test2():
+def riccatiIteration(A, B, Q, R):
 
+def calc_gain():
+
+
+def test2():
     timelist = []
     datalist = []
+    x = np.array([[0,0,0,0]]).T
+    K = calc_gain()
+    for i in range(10000):
+        timelist.append(i * dt)
+        u = np.dot(K, x)[0][0]
+        x = calc_next(x, u)
+        datalist.append(x[2][0])
+    plt.plot(timelist, datalist, label="test2")
+    plt.legend()
+    plt.show()
+
     
 
 test1()
